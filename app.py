61c15a8bc72bf4211b566215a6e107aec845da39
Chris-code240 <duah.marfochristian@gmail.com>
2022-09-17 03:48:01 +0100

    Touched the POST endpoint, worked on modals

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from models import *
 app = Flask(__name__)
 app.config.from_object('config')
 
-current_years = ["2021","2020","2019","2018","2017"]
+current_years = ["2022","2021","2020","2019","2018","2017"]
 # APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 # UPLOAD_FOLDER = os.path.join(APP_ROOT,'static','questions')
 
@@ -27,27 +27,6 @@ setuDb(app)
 # Run Once
 # delete_all_create_all()
 
-class UploadForm(FlaskForm):
-    file = wtforms.FileField("file")
-    upload = wtforms.SubmitField("upload")
-
-@app.route('/up-form',methods=['POST','GET'])
-def uploa():
-    form = UploadForm()
-    if form.validate_on_submit():
-        file = UploadForm().file.data #grab the file
-        print((file))
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['ULOPAD_FOLDER'],secure_filename(file)))
-         # save file
-
-    return render_template('pages/up-form.html',form=form)
-
-@app.route('/loaded')
-def done():
-    return "File Saved"
-
-
-
 @app.route('/')
 def home():
     return render_template('pages/home.html',nav=True,title='Home',help=False,FAQ=True,start_btn=True,year_selection=False)
@@ -58,8 +37,7 @@ def user_select():
 
 @app.route('/select-question')
 def user_select_question():
-    return render_template('pages/select-paper.html',nav=True,title="Select question",HELP=True,start_btn=False,year_selection=True,year="2019",years=current_years)
-
+    return render_template('pages/select-paper.html',nav=True,title="Select question",HELP=True,start_btn=False,year_selection=True,year="2022",years=current_years)
 
 @app.route('/upload')
 def new_question():
@@ -71,6 +49,7 @@ def new_question():
 @app.route('/questions')
 @app.route('/question')
 def get_questions():
+    try:
         req = request.args
         program = req['program']
         level = req['level']
@@ -79,40 +58,36 @@ def get_questions():
         questions = [
                 {"id":ques.id,"file":send_file(BytesIO(ques.file),download_name=ques.file_name,as_attachment=False),"semester":ques.semester,"file_name":ques.file_name} for ques in Question.query.filter(Question.year == year,Question.program == program, Question.course_name == course, Question.level == level).all()
                 ]
-
-         
         return render_template('pages/get_questions.html',nav=True,title="Questions",FAQ=True,year_selection=True,year=year,years=current_years,data={'course_title':course,"questions":questions,"total":len(questions)})
         # return questions[0]['file']
-   
-@app.route('/test-form')
-def test():
-    return render_template('pages/test_form.html')
+    except:
+        abort(422)
 
-@app.route('/test-form',methods=['POST'])
-def post_form():
-    print(request)
-    return jsonify({})
-
+        
+"""Add new question to the database"""  
 @app.route('/questions',methods=['POST'])
 @app.route('/question',methods=['POST'])
 def post_question():
-    file = request.files['question-paper'].read()
-    form = request.form
-    year = form['year']
-    program = form['program']
-    course = form['course']
-    file_name = request.files['question-paper'].filename
-    level = form['level']
-    semester = form['semester']
+    try:
+        file = request.files['file'].read()
+        form = request.form
+        year = form['year']
+        program = form['program']
+        course = form['course']
+        file_name = request.files['file'].filename
+        level = form['level']
+        semester = form['semester']
 
-    question = Question(course,year,program,file,file_name,level,semester)
-    question.insert()
-    id = question.id
-    b_file = question.file
+        question = Question(course,year,program,file,file_name,level,semester)
+        question.insert()
+        res = {"success":True,"message":"Question uploaded successfully"}
+        # return render_template('pages/upload.html',title='Upload',HELP=True,start_btn=True)
+        return jsonify(res)
+    except:
+        return jsonify({"success":False})
+        # abort(422)
 
-    return url_for('new_question')
-
-# sever image to frontend
+"""Serve document to a view"""
 @app.route('/show-question/<int:ques_id>')
 def show(ques_id):
     question = Question.query.filter(Question.id == ques_id).one_or_none()
@@ -121,7 +96,7 @@ def show(ques_id):
         return send_file(BytesIO(question.file),download_name=question.file_name,as_attachment=False)
     abort(404)
 
-
+"""View selected document"""
 @app.route('/selected/<int:ques_id>')
 def get_selected(ques_id):
     question = Question.query.filter(Question.id == ques_id).one_or_none()
@@ -129,5 +104,11 @@ def get_selected(ques_id):
         return render_template('pages/selected.html',course_title=question.course_name,id=question.id,title=question.course_name)
     abort(404)
 
+
+@app.route('/test-post',methods=['POST'])
+def test():
+    print(request.form['year'])
+    print(not request.files['file'])
+    return jsonify({"success":True})
 
 
