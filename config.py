908763f868d0325fcb7e1fe6577f819aaa810d37
Chris-code240 <duah.marfochristian@gmail.com>
2022-10-24 10:10:48 +0100

    tweaked config file

--- config.py
+++ config.py
@@ -11,7 +11,8 @@ DB_PASSWORD = 'Liukangs240'
 
 
 # IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:{0}@localhost:5432/{1}'.format(DB_PASSWORD,DB_NAME)
+# SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:{0}@localhost:5432/{1}'.format(DB_PASSWORD,DB_NAME)
+SQLALCHEMY_DATABASE_URI = 'postgres://zdhildnqsmdqpf:dc677944694cc881e333f723cd45907656436264ab1f610d288ca1b8641e9545@ec2-54-163-34-107.compute-1.amazonaws.com:5432/dckdou76bps9ho'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 SESSION_COOKIE_DOMAIN = False
