3bb4fbd0f3aa9ec66901c67bc003410f89538b5b
Christian Duah <82686181+Chris-code240@users.noreply.github.com>
2023-02-13 22:12:12 +0000

    changed connection string to internal

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ DB_PASSWORD = 'Liukangs240'
 
 
 # IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgres://pasquo_user:mf7vKphODwSEAUSkJKQT3hEDHjeX0fbr@dpg-cfl3rgla49903fj1bh8g-a.oregon-postgres.render.com/pasquo'
+SQLALCHEMY_DATABASE_URI = 'postgres://pasquo_user:mf7vKphODwSEAUSkJKQT3hEDHjeX0fbr@dpg-cfl3rgla49903fj1bh8g-a/pasquo'
 # SQLALCHEMY_DATABASE_URI = 'postgres://zdhildnqsmdqpf:dc677944694cc881e333f723cd45907656436264ab1f610d288ca1b8641e9545@ec2-54-163-34-107.compute-1.amazonaws.com:5432/dckdou76bps9ho'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
